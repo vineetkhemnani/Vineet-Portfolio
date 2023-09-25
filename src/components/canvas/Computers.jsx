324ccc786d7a30a5a7a67928964c42cfed6f234a
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-09-26 00:07:29 +0530

    mobile friendly optimization

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,11 +1,24 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import React, { Suspense,useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from '../Loader'
+
+const VIEWPORT_WIDTH = 500 // Adjust to your desired viewport width
+const COMPUTER_SCALE_MOBILE = 0.5
+const COMPUTER_SCALE_DESKTOP = 0.75
+const COMPUTER_POSITION_MOBILE = [0, -3, -2.2]
+const COMPUTER_POSITION_DESKTOP = [0, -3.25, -1.5]
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
+
+  const computerScale = isMobile
+    ? COMPUTER_SCALE_MOBILE
+    : COMPUTER_SCALE_DESKTOP
+  const computerPosition = isMobile
+    ? COMPUTER_POSITION_MOBILE
+    : COMPUTER_POSITION_DESKTOP
 
   return (
     <mesh>
@@ -21,8 +34,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75} // Adjust the scale for mobile
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]} // Adjust the position for mobile
+        scale={computerScale}
+        position={computerPosition}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -33,7 +46,7 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    const mediaQuery = window.matchMedia(`(max-width: ${VIEWPORT_WIDTH}px)`)
     setIsMobile(mediaQuery.matches)
 
     const handleMediaQueryChange = (event) => {
@@ -58,7 +71,7 @@ const ComputersCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          enableZoom={!isMobile} // Disable zoom for mobile
+          enableZoom={!isMobile}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
