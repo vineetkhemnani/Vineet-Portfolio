da1771faa86348ea9cb7f95a2e0a46080e0e28dc
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-09-25 20:23:53 +0530

    added contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,35 +33,38 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        'service_xdw577p',
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'template_28ujgbp',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: 'Vineet Khemnani',
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: 'vineetkhemnani09@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '7p3O64cKkLiUYo8Fp'
       )
       .then(
         () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          setLoading(false)
+          alert('Thank you. I will get back to you as soon as possible.')
 
           setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
+            name: '',
+            email: '',
+            message: '',
+          })
         },
         (error) => {
-          setLoading(false);
-          console.error(error);
+          setLoading(false)
+          console.error(error)
 
-          alert("Ahh, something went wrong. Please try again.");
+          alert('Ahh, something went wrong. Please try again.')
         }
-      );
+      )
   };
 
   return (
